67592efafddf7aaf15f7fe43ba1610e7f90b2837
LeinadAsid <danynovaisdias@outlook.com>
2023-05-07 19:50:42 -0300

    changing max-file size

--- chat-app-backend/src/gateway/ws-gateway/ws-gateway.gateway.ts
+++ chat-app-backend/src/gateway/ws-gateway/ws-gateway.gateway.ts
@@ -10,7 +10,7 @@ import {
 import { Socket } from 'socket.io';
 import { Message } from './interfaces/ws-gateway.interface';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: true, maxHttpBufferSize: 1e7 })
 export class WsGatewayGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
